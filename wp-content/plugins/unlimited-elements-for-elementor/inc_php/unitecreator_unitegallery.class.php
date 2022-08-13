2d417fedeac9db60eeca01a29856cf1214bd04ae
zeroxerxes <brandonasah11@gmail.com>
2022-08-14 00:37:00 +0100

    init

---
+++ wp-content/plugins/unlimited-elements-for-elementor/inc_php/unitecreator_unitegallery.class.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package Unlimited Elements
+ * @author unlimited-elements.com
+ * @copyright (C) 2021 Unlimited Elements, All Rights Reserved. 
+ * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ * */
+defined('UNLIMITED_ELEMENTS_INC') or die('Restricted access');
+
+class UniteCreatorUniteGallery{
+	
+	
+	/**
+	 * get gallery js settings from data
+	 */
+	public function getUniteGalleryJsSettings($data,UniteCreatorAddon $addon){
+		
+		dmp($data);
+		dmp("get settings from data");
+		//exit();
+		
+	}
+	
+}
